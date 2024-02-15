ef81ea5ea8bebe775c0e60c269ca29cc1db94b73
Mooncakeproject <ao.carey@hotmail.com>
2024-02-15 12:51:53 +0700

    created bookings and reviews routes

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from 'express'
 const app = express()
 
+// Import the users router module
+import reviewsRouter from './routes/reviewsRoutes.js'
+app.use(reviewsRouter)
+import bookingsRouter from './routes/bookingsRoutes.js'
+app.use(bookingsRouter)
+
+// Tell the app to use the user router
 app.listen(4100, () => {
   console.log('Airbnb API ready on localhost:4100')
 })
