f7919c2927e1fcbe322565fdd42a8eae3994337f
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-15 12:41:09 +0700

    create photosRoutes

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from 'express'
 const app = express()
 
+// Import the photos router module
+import photosRouter from './routes/photosRoutes.js'
+
+// Tell the app to use the photos router
+app.use(photosRouter)
+
 app.listen(4100, () => {
   console.log('Airbnb API ready on localhost:4100')
 })
---
+++ routes/photosRoutes.js
@@ -0,0 +1,15 @@
+// import router
+import { Router } from 'express'
+const router = Router()
+
+// create photos routes
+router.get('/photos', (req, res) => {
+  const photos = [
+    { id: 1, photo: 'https://random.imagecdn.app/500/150' },
+    { id: 2, photo: 'https://random.imagecdn.app/500/150' }
+  ]
+  res.send(photos)
+})
+
+// Export the router
+export default router
