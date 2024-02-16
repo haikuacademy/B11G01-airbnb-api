71bea1b954ca0f799d39b62f187a487f8c7799de
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-16 12:09:14 +0700

    created query for /photos

--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -1,22 +1,27 @@
 // import router
 import { Router } from 'express'
 const router = Router()
+import db from '../db.js' // import the database connection
 
 // create photos routes
-router.get('/photos', (req, res) => {
-  const photos = [
-    { id: 1, photo: 'https://random.imagecdn.app/500/150' },
-    { id: 2, photo: 'https://random.imagecdn.app/500/150' }
-  ]
-  res.json(photos)
+router.get('/photos', async (req, res) => {
+  // don't forget async
+  try {
+    const { rows } = await db.query('SELECT * FROM pictures') // query the database
+    console.log(rows)
+    res.json(rows) // respond with the data
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
 })
 
 //create /photos/1
-router.get('/photos/1', (req, res) => {
-  const photos1 = { id: 1, photo: 'https://random.imagecdn.app/500/150' }
+// router.get('/photos/1', (req, res) => {
+//   const photos1 = { id: 1, photo: 'https://random.imagecdn.app/500/150' }
 
-  res.json(photos1)
-})
+//   res.json(photos1)
+// })
 
 // Export the router
 export default router
