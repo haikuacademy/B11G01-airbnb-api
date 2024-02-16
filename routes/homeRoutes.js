363feb326fbd062a2048cd0e21705ca528d9b746
AlvesDv <146094336+AlvesDv@users.noreply.github.com>
2024-02-16 12:36:03 +0700

    Fetching single user for home and users

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -13,4 +13,18 @@ router.get('/home', async (req, res) => {
   }
 })
 
+// Define a GET route for fetching a single user
+router.get('/home/1', async (req, res) => {
+  try {
+    const { rows } = await db.query(
+      'SELECT * FROM houses WHERE houses.house_ID = 1'
+    ) // query the database
+    console.log(rows)
+    res.json(rows) // respond with the data
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
+})
+
 export default router
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -13,4 +13,16 @@ router.get('/users', async (req, res) => {
   }
 })
 
+// Define a GET route for fetching a single user
+router.get('/users/1', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM users WHERE users.user_id = 1') // query the database
+    console.log(rows)
+    res.json(rows) // respond with the data
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
+})
+
 export default router
