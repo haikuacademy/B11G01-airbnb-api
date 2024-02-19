974fc3abd2f5523b0b8e4394c930af621966a5c0
AlvesDv <146094336+AlvesDv@users.noreply.github.com>
2024-02-19 14:13:03 +0700

    Rearranging and correcting error message on users and houses

--- routes/housesRoutes.js
+++ routes/housesRoutes.js
@@ -13,19 +13,23 @@ router.get('/houses', async (req, res) => {
   }
 })
 
-// Define a GET route for fetching a single house
+// // Define a GET route for fetching a single house
 router.get('/houses/:houseId', async (req, res) => {
   try {
+    let houseId = Number(req.params.houseId)
+    if (!houseId) {
+      throw new Error('Please insert a number')
+    }
     const { rows } = await db.query(
       `SELECT * FROM houses WHERE house_id = ${req.params.houseId}`
     )
     if (rows.length === 0) {
-      return res.json({ error: 'House not found' })
+      throw new Error('House not found')
     }
     res.json(rows)
   } catch (err) {
     console.error(err.message)
-    res.json({ error: 'Internal server error' })
+    res.json(err.message)
   }
 })
 
