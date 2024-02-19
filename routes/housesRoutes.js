bd6a0ca56b8ba9281eabc642ee60178f86bfddd7
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-19 21:15:37 +0700

    Update the house routes with queries for location

--- routes/housesRoutes.js
+++ routes/housesRoutes.js
@@ -2,18 +2,7 @@ import { Router } from 'express'
 const router = Router()
 import db from '../db.js' // import the database connection
 
-router.get('/houses', async (req, res) => {
-  try {
-    const { rows } = await db.query('SELECT * FROM houses') // query the database
-    console.log(rows)
-    res.json(rows) // respond with the data
-  } catch (err) {
-    console.error(err.message)
-    res.json(err)
-  }
-})
-
-// Define a GET route for fetching a single house
+// Define a GET route for house_id with params
 router.get('/houses/:houseId', async (req, res) => {
   try {
     const { rows } = await db.query(
@@ -29,4 +18,31 @@ router.get('/houses/:houseId', async (req, res) => {
   }
 })
 
+//Update the /houses route with queries
+router.get('/houses', async (req, res) => {
+  try {
+    //query for houses
+    let queryString = 'SELECT * FROM houses'
+    //query for location
+    if (req.query.location) {
+      queryString += ` WHERE location = '${req.query.location}'`
+    }
+    const { rows } = await db.query(queryString)
+    res.json(rows)
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
+})
+
+// router.get('/houses', async (req, res) => {
+//   try {
+//     const { rows } = await db.query('SELECT * FROM houses') // query the database
+//     console.log(rows)
+//     res.json(rows) // respond with the data
+//   } catch (err) {
+//     console.error(err.message)
+//     res.json(err)
+//   }
+// })
 export default router
