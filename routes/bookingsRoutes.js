951ce7d3339a7c0f22096817b045f8a8369bfaf3
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-19 14:05:20 +0700

    GET bookings/1 with params

--- routes/bookingsRoutes.js
+++ routes/bookingsRoutes.js
@@ -14,18 +14,38 @@ router.get('/bookings', async (req, res) => {
   }
 })
 
-router.get('/bookings/11', async (req, res) => {
-  // don't forget async
+// params for GET bookings/1
+router.get('/bookings/:bookingId', async (req, res) => {
   try {
+    let bookingId = Number(req.params.bookingId)
+    if (!bookingId) {
+      throw new Error('Please insert a number')
+    }
     const { rows } = await db.query(
-      'SELECT * FROM bookings WHERE booking_id = 11'
-    ) // query the database
-    console.log(rows)
-    res.json(rows) // respond with the data
+      `SELECT * FROM bookings WHERE booking_id = ${req.params.bookingId}`
+    )
+    if (rows.length === 0) {
+      throw new Error('Booking not found')
+    }
+    res.json(rows)
   } catch (err) {
     console.error(err.message)
-    res.json(err)
+    res.json(err.message)
   }
 })
+
+// router.get('/bookings/11', async (req, res) => {
+//   // don't forget async
+//   try {
+//     const { rows } = await db.query(
+//       'SELECT * FROM bookings WHERE booking_id = 11'
+//     ) // query the database
+//     console.log(rows)
+//     res.json(rows) // respond with the data
+//   } catch (err) {
+//     console.error(err.message)
+//     res.json(err)
+//   }
+// })
 
 export default router
