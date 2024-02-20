0cf9a508b248da55c21de672638d2d05c266b4c5
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-20 12:32:02 +0700

    post req for sign up

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
 const router = Router()
+import db from '../db.js' // import the database connection
 
 //authRoutes for /signup
-router.get('/signup', (req, res) => {
-  let signUpMessage = 'This is from Signup'
-  res.send(signUpMessage)
+router.post('/signup', (req, res) => {
+  const insertion = await db.query(`
+    INSERT INTO users (first_name, last_name)
+    VALUE ('${req.body.first_name}', '${req.body.last_name}')
+  `)
+  res.json(insertion)
 })
 
 //authRoutes for /login
