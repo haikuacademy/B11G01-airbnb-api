1bfe5a4db781f02fa75542e67a6369011c969433
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-20 13:14:17 +0700

    post request for sign up

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,12 +3,22 @@ const router = Router()
 import db from '../db.js' // import the database connection
 
 //authRoutes for /signup
-router.post('/signup', (req, res) => {
-  const insertion = await db.query(`
-    INSERT INTO users (first_name, last_name)
-    VALUE ('${req.body.first_name}', '${req.body.last_name}')
-  `)
-  res.json(insertion)
+router.post('/signup', async (req, res) => {
+  try {
+    const { first_name, last_name, email, password } = req.body
+    console.log(req.body, first_name, last_name)
+    const queryString = `
+      INSERT INTO users (first_name, last_name, email, password)
+      VALUES ('${first_name}', '${last_name}', '${email}', '${password}')
+      RETURNING *
+    `
+    console.log(queryString)
+    const { rows } = await db.query(queryString)
+    res.json(rows)
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
 })
 
 //authRoutes for /login
