a1c44c30fecb2f2a2d9e2e3ff3e77485e1738472
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-21 12:02:41 +0700

    patch for changing user's password

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -41,6 +41,9 @@ router.patch('/users/change-first_name/:userId', async (req, res) => {
     SET first_name = '${req.body.first_name}'
     WHERE user_id = ${req.params.userId}
     `)
+    if (!req.body.first_name) {
+      throw new Error('Please put your first name')
+    }
     res.json(rows)
   } catch (err) {
     console.error(err.message)
@@ -56,6 +59,9 @@ router.patch('/users/change-last_name/:userId', async (req, res) => {
     SET last_name = '${req.body.last_name}'
     WHERE user_id = ${req.params.userId}
     `)
+    if (!req.body.last_name) {
+      throw new Error('Please put your last name')
+    }
     res.json(rows)
   } catch (err) {
     console.error(err.message)
@@ -71,6 +77,27 @@ router.patch('/users/change-email/:userId', async (req, res) => {
     SET email = '${req.body.email}'
     WHERE user_id = ${req.params.userId}
     `)
+    if (!req.body.email) {
+      throw new Error('Please put your email')
+    }
+    res.json(rows)
+  } catch (err) {
+    console.error(err.message)
+    res.json(err.message)
+  }
+})
+
+//patch for changing user's password
+router.patch('/users/change-password/:userId', async (req, res) => {
+  try {
+    const { rows } = await db.query(`
+    UPDATE users
+    SET password = '${req.body.password}'
+    WHERE user_id = ${req.params.userId}
+    `)
+    if (!req.body.email) {
+      throw new Error('Please put your password')
+    }
     res.json(rows)
   } catch (err) {
     console.error(err.message)
