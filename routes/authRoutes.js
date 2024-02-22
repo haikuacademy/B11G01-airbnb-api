8a0174052f2a94d057ac8f74e86dc439d30a82cb
AlvesDv <146094336+AlvesDv@users.noreply.github.com>
2024-02-22 12:50:46 +0700

    installing jsonwebtoken and importing it to authroutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,8 @@ import { Router } from 'express'
 const router = Router()
 import db from '../db.js' // import the database connection
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import jwtSecret from '../secrets.js'
 
 //authRoutes for /signup
 
