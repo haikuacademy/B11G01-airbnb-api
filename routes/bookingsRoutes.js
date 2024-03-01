2ef405dffb42f785ba4b5d2b4ac170b92d897b9a
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-03-01 09:03:10 +0700

    Update bookingsRoutes.js

--- routes/bookingsRoutes.js
+++ routes/bookingsRoutes.js
@@ -5,15 +5,24 @@ import jwt from 'jsonwebtoken'
 import { jwtSecret } from '../secrets.js'
 
 // params for GET bookings/1
-
 router.get('/bookings/:bookingId', async (req, res) => {
+  //check if the user is logged in
+  const token = req.cookies.jwt
+  let decoded
+  try {
+    decoded = jwt.verify(token, jwtSecret)
+  } catch (e) {
+    res.json({ error: 'Invalid authentication token' })
+    return
+  }
+
   try {
     let bookingId = Number(req.params.bookingId)
     if (!bookingId) {
       throw new Error('Please insert a number')
     }
     const { rows } = await db.query(
-      `SELECT * FROM bookings WHERE booking_id = ${req.params.bookingId}`
+      `SELECT * FROM bookings WHERE user_id = ${decoded.user_id} AND booking_id = ${req.params.bookingId}`
     )
     if (rows.length === 0) {
       throw new Error(`No Booking found with ID ${req.params.bookingId}`)
@@ -26,15 +35,26 @@ router.get('/bookings/:bookingId', async (req, res) => {
 })
 
 //Update the /bookings route with queries
-
 router.get('/bookings', async (req, res) => {
+  //check if the user is logged in
+  const token = req.cookies.jwt
+  let decoded
   try {
-    let queryBookings =
-      'SELECT * FROM bookings ORDER BY booking_start_date DESC'
-    if (req.query.user) {
-      queryBookings = `SELECT * FROM bookings WHERE user_id = ${req.query.user} ORDER BY booking_start_date DESC`
-    }
+    decoded = jwt.verify(token, jwtSecret)
+  } catch (e) {
+    res.json({ error: 'Invalid authentication token' })
+    return
+  }
+
+  try {
+    //only returns the bookings made by the requesting user
+    let queryBookings = `
+    SELECT * FROM bookings WHERE user_id = ${decoded.user_id} ORDER BY booking_start_date DESC
+    `
     const { rows } = await db.query(queryBookings)
+    if (rows.length === 0) {
+      throw new Error('not authorized')
+    }
     res.json(rows)
   } catch (err) {
     console.error(err.message)
@@ -43,17 +63,16 @@ router.get('/bookings', async (req, res) => {
 })
 
 // POST bookings
-
 router.post('/bookings', async (req, res) => {
   const token = req.cookies.jwt
   const {
-        user_id,
-        house_id,
-        booking_start_date,
-        booking_end_date,
-        price,
-        message_to_host
-      } = req.body
+    user_id,
+    house_id,
+    booking_start_date,
+    booking_end_date,
+    price,
+    message_to_host
+  } = req.body
   let decoded
   try {
     decodedToken = jwt.verify(token, jwtSecret)
@@ -73,11 +92,11 @@ router.post('/bookings', async (req, res) => {
     }
     const queryString2 = `
       INSERT INTO bookings (user_id, house_id, booking_start_date, booking_end_date, price, message_to_host)
-      VALUES (${user_id}, ${house_id}, '${booking_start_date}', '${booking_end_date}', ${price}, '${message_to_host}')
+      VALUES (${decoded.user_id}, ${house_id}, '${booking_start_date}', '${booking_end_date}', ${price}, '${message_to_host}')
       RETURNING *
-    ` 
-      const { rows } = await db.query(queryString2)
-      res.json(rows)
+    `
+    const { rows } = await db.query(queryString2)
+    res.json(rows)
   } catch (err) {
     res.json({ error: err.message })
   }
@@ -97,6 +116,21 @@ router.delete('/bookings/:bookingId', async (req, res) => {
     res.json({ error: 'Please insert a valid data' })
   }
 })
+
+// DELETE bookings
+// router.delete('/bookings/:bookingId', async (req, res) => {
+//   try {
+//     const { rowCount } = await db.query(`
+//     DELETE FROM bookings WHERE booking_id = ${req.params.bookingId}`)
+//     if (!rowCount) {
+//       throw new Error('Delete Failed')
+//     }
+//     res.json(rowCount)
+//   } catch (err) {
+//     console.error(err)
+//     res.json({ error: 'Please insert a valid data' })
+//   }
+// })
 export default router
 
 // Similarly, update the /bookings route so that, by default and without any URL query, it responds with the list of bookings
